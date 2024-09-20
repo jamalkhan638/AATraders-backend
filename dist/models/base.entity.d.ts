9305a0698fca9bd1de55aa4800df25fe0e15848f
jamalkhan638 <40866562+jamalkhan638@users.noreply.github.com>
2024-09-20 15:39:21 +0500

    Delete dist directory

--- dist/models/base.entity.d.ts
+++
@@ -1,5 +0,0 @@
-export declare abstract class BaseEntity {
-    created_at: any;
-    updated_at: any;
-    deleted_at?: any;
-}
--- dist/types/token-payload.type.d.ts
+++
@@ -1,6 +0,0 @@
-export type TokenPayload = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-};
