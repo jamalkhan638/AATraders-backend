d925f7e46bebdb6d7609f4595f57efc0ec9e35f9
jamalAhmad6381 <124151436+jamalAhmad6381@users.noreply.github.com>
2024-09-23 20:20:26 +0500

    G

--- dist/config/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    environment: process.env.EXPRESS_ENVIRONMENT,
-    port: process.env.EXPRESS_PORT,
-    jwtSecret: process.env.JWT_SECRET,
-};
-//# sourceMappingURL=app.js.map
